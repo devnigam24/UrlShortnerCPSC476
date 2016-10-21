ec992dde2e85631761b8d5a6baac235aeafc857b
Devvrat Nigam <devnigam24@gmail.com>
2016-10-21 16:04:45 -0700

    Removed annotation and updated for Model 2 architecture

--- src/main/java/com/fullerton/edu/cpsc/cpsc476/servlets/SignUpServlet.java
+++ src/main/java/com/fullerton/edu/cpsc/cpsc476/servlets/SignUpServlet.java
@@ -3,59 +3,70 @@ package com.fullerton.edu.cpsc.cpsc476.servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.fullerton.edu.cpsc.cpsc476.Util.DAOUtil;
+import com.fullerton.edu.cpsc.cpsc476.Util.ErrorAndMessages;
+import com.fullerton.edu.cpsc.cpsc476.Util.ShowErrorPageUtil;
 import com.fullerton.edu.cpsc.cpsc476.pojo.NewUserDetails;
 
-@WebServlet(
-			name="SignUpServlet",
-			urlPatterns = {"/SignUpServlet"}
-		)
 public class SignUpServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) {
-		doPost(req, res);
+		ShowErrorPageUtil.redirectToErrorPage(req, res, ErrorAndMessages.inforCompromised);
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse res) {
-		String username = "", password = "", confirm = "", email = "";
+		String username = "";
+		String password = "";
+		String confirm = "";
+		String email = "";
+
 		if (req.getParameter("username") != null && req.getParameter("username") != "") {
 			username = req.getParameter("username");
+			req.setAttribute("username", username);
+		} else {
+			ShowErrorPageUtil.redirectToErrorPage(req, res, "signUp.jsp", ErrorAndMessages.usernameNull);
+			return;
 		}
 		if (req.getParameter("password") != null && req.getParameter("password") != "") {
 			password = req.getParameter("password");
+		} else {
+			ShowErrorPageUtil.redirectToErrorPage(req, res, "signUp.jsp", ErrorAndMessages.passwordNull);
+			return;
 		}
 		if (req.getParameter("cpassword") != null && req.getParameter("cpassword") != "") {
 			confirm = req.getParameter("cpassword");
 
+		} else {
+			ShowErrorPageUtil.redirectToErrorPage(req, res, "signUp.jsp", ErrorAndMessages.passwordNull);
+			return;
 		}
 		if (req.getParameter("email") != null && req.getParameter("email") != "") {
 			email = req.getParameter("email");
-		}		
+			req.setAttribute("email", email);
+		} else {
+			ShowErrorPageUtil.redirectToErrorPage(req, res, "signUp.jsp", ErrorAndMessages.emailNull);
+			return;
+		}
 		if (password.equals(confirm)) {
 			try {
 				NewUserDetails newUser = new NewUserDetails(username, email, password);
-				Boolean isDataStored = DAOUtil.storeUserInDB(req, res, newUser);
-				if(isDataStored){
-					HttpSession session = req.getSession();
-					session.setAttribute("userInsession", newUser);
-					req.getRequestDispatcher("welcome.jsp").forward(req, res);
-				}
+				HttpSession session = req.getSession();
+				session.setAttribute("userInsession", newUser);
+				req.getRequestDispatcher("welcome.jsp").forward(req, res);
 			} catch (ServletException e) {
-				System.out.println(e);
+				ShowErrorPageUtil.redirectToErrorPage(req, res, e.getMessage());
+				return;
 			} catch (IOException e) {
-				System.out.println(e);
+				ShowErrorPageUtil.redirectToErrorPage(req, res, e.getMessage());
+				return;
 			}
 		} else {
-			req.setAttribute("name", username);
-			req.setAttribute("email", email);
 			req.setAttribute("passwordDonotMatch", "true");
 			try {
 				req.getRequestDispatcher("signUp.jsp").forward(req, res);
